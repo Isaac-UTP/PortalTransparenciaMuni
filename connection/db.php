7c61d88cc8ef15243cd0cd6ec813d9cb7b8aae10
Isaac <isaactakamura107@gmail.com>
2025-01-08 22:45:20 -0500

    ajustando la conexion

--- connection/db.php
+++ connection/db.php
@@ -1,9 +1,9 @@
 <?php
 //Archivo de Conexión a la BD
 $host = "localhost";
-$dbname = "nombre_base_datos";
-$user = "usuario";
-$password = "contraseña";
+$dbname = "MUNI";
+$user = "root";
+$password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
