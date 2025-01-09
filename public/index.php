a730c6797569e58f0019ddd174886aa8b3148923
Isaac <isaactakamura107@gmail.com>
2025-01-09 00:10:42 -0500

    boostrap en boton y tabla

--- public/index.php
+++ public/index.php
@@ -39,11 +39,28 @@ $documentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Documentos Municipales</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/modal.css">
+    <!-- Importacion Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <!-- Importacion de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
     <h1>Documentos Municipales</h1>
-    <button id="openModalBtn">Subir Archivo</button>
+    <div class="x-grid gap-2 d-flex justify-content-center">
+        <button id="openModalBtn" class="btn btn-primary" type="button">Subir Archivo</button>
+    </div>
+
 
     <!-- Modal -->
     <div id="uploadModal" class="modal">
@@ -81,8 +98,9 @@ $documentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <input type="text" name="busqueda" placeholder="Palabra clave" value="<?= htmlspecialchars($busqueda) ?>">
         <button type="submit">Buscar</button>
     </form>
-
-    <table>
+    <!-- Tabla de Documentos -->
+    <table class="table table-bordered" style="width: 65%;">
+        <!-- Tipo de tabla + Tamaño de la tabla -->
         <thead>
             <tr>
                 <th>#</th>
