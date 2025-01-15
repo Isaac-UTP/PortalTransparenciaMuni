e778ae68cc6375c67dbca3c46ba61f03ded02d51
Isaac <isaactakamura107@gmail.com>
2025-01-14 21:54:55 -0500

    boostrap al index

--- public/index.php
+++ public/index.php
@@ -19,35 +19,45 @@ $documentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Documentos Subidos</title>
     <link rel="stylesheet" href="path/to/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div class="container">
-        <h1>Documentos Subidos</h1>
-        <a href="subir_documento.php" class="btn btn-primary mb-3">Subir Documento</a>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Tipo</th>
-                    <th>Año</th>
-                    <th>Número</th>
-                    <th>Descripción</th>
-                    <th>Enlace</th>
-                </tr>
-            </thead>
-            <tbody id="documentosTableBody">
-                <?php foreach ($documentos as $documento): ?>
-                    <tr>
-                        <td><?= htmlspecialchars($documento['tipos']) ?></td>
-                        <td><?= htmlspecialchars($documento['anno']) ?></td>
-                        <td><?= htmlspecialchars($documento['numero']) ?></td>
-                        <td><?= htmlspecialchars($documento['descripcion']) ?></td>
-                        <td><a href="<?= htmlspecialchars($documento['link']) ?>" target="_blank">Ver</a></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col-lg-12 text-left">
+                <h1>Documentos Subidos</h1>
+                <a href="subir_documento.php" class="btn btn-primary mb-3">Subir Documento</a>
+                <table class="table table-striped table-bordered" style="font-size:12px;">
+                    <thead>
+                        <tr>
+                            <th>Tipo</th>
+                            <th>Año</th>
+                            <th>Número</th>
+                            <th>Descripción</th>
+                            <th>Enlace</th>
+                        </tr>
+                    </thead>
+                    <tbody id="documentosTableBody">
+                        <?php foreach ($documentos as $documento): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($documento['tipos']) ?></td>
+                                <td><?= htmlspecialchars($documento['anno']) ?></td>
+                                <td><?= htmlspecialchars($documento['numero']) ?></td>
+                                <td><?= htmlspecialchars($documento['descripcion']) ?></td>
+                                <td><a href="<?= htmlspecialchars($documento['link']) ?>" target="_blank">Ver</a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
+
 
     <script src="path/to/bootstrap.bundle.js"></script>
     <script>
