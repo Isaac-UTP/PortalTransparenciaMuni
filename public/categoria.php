7de19195a1df313478bb13daefacc711c872873f
Isaac <isaactakamura107@gmail.com>
2025-02-21 08:53:12 -0500

    creacion de css, para evitar codigo spaguetti

--- public/categoria.php
+++ public/categoria.php
@@ -60,6 +60,7 @@ $paginacion = generarPaginacion($totalPages, $page);
     <title>Ver Categorías</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/categoria.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -69,16 +70,6 @@ $paginacion = generarPaginacion($totalPages, $page);
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
-    <style>
-        .content {
-            margin-left: 250px;
-            padding: 20px;
-        }
-
-        .raleway-font {
-            font-family: 'Raleway', sans-serif;
-        }
-    </style>
 </head>
 
 <body>
